b6fceebc2f0031fdc8813855bf2e92d1831d8c59
bruere_s <sebastien.bruere@epitech.eu>
2016-12-06 10:58:03 +0100

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,35 @@ var titleMission = ['Mission école pour nos super-héros !','Une super-chambre
 var subtitleMission = ['Même à l’hôpital, un super-héros doit continuer à apprendre plein de choses ! Dans la nouvelle salle de classe, grâce à des installations adaptées, une enseignante spécialisée enseignera à tous les niveaux, pour que nos jeunes patients ne perdent pas le lien avec l’école.','Adaptés pour les patients à mobilité réduite, prévues pour accueillir un proche, les chambres individuelles seront le quartier général de chaque super-héros ! Ils pourront s’y reposer après les traitements, lire, rêver… pour reprendre des forces dans les meilleures conditions ! ','Enfants, bébés, adolescents : nos super-héros ont tous des besoins différents ! Pour favoriser leur épanouissement, notre nouvelle unité pédiatrique, adolescents et jeunes adultes comprendra des espaces de vie et de loisirs adaptés à chaque âge et ouverts sur l’extérieur : biberonnerie et nurserie pour les tout-petits, espaces de loisirs pour se retrouver autour d’activités ludiques et culturelles pour les plus grands.'];
 var imgMission = ['https://s3.amazonaws.com/heroku-adfinitas-campaign/COL-lp-NouvUnit-2016/mission-1.png','https://s3.amazonaws.com/heroku-adfinitas-campaign/COL-lp-NouvUnit-2016/mission-2.png','https://s3.amazonaws.com/heroku-adfinitas-campaign/COL-lp-NouvUnit-2016/mission-3.png'];
 
+$(window).resize( function() {
+	adaptSizeSlideUn();
+
+});
+
 $(document).ready ( function() {
 	preload(imgMission);
 
 	$('.block-titre-slider').first().css({'background-color':'#b00076'});
+
+	setTimeout( function(){
+		$('#img-header-1').fadeOut( 2000, function() {
+		});
+		$('#img-header-2').fadeIn( 2000, function() {
+			$('#btn-univers').fadeIn( "slow", function() {
+			});
+		});
+	}  , 1500 );
+
+	adaptSizeSlideUn();
+	function adaptSizeSlideUn() {
+		if ($('#img-header-1').css('display') != 'none')
+			var height = $('#img-header-1').height();
+		else
+			var height = $('#img-header-2').height();
+
+		console.log(height);
+		$('.row.slide.un').css('height',height + 100);
+	}
 });
 
 $('.block-titre-slider').first().hover( function() {
